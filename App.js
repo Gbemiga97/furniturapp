cb1d50b5e0af8760190b643f6d2a3c22609614a6
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-04-22 12:55:07 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
+
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    semiBold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+    extraBold: require("./assets/fonts/Poppins-ExtraBold.ttf")
+  })
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
